db1e4e0414a1c33fbaddb7d99269c87972fea0ea
VarGhimoyan2004 <154809343+VarGhimoyan2004@users.noreply.github.com>
2024-10-21 11:18:36 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-const elevators = [  { id: 'elevator1', currentFloor: 1 },
-  { id: 'elevator2', currentFloor: 1 },  { id: 'elevator3', currentFloor: 1 }
+const elevators = [
+  { id: 'elevator1', currentFloor: 1 },
+  { id: 'elevator2', currentFloor: 1 },
+  { id: 'elevator3', currentFloor: 1 }
 ];
-function moveToFloor(elevator, floor) { 
-  const elevatorDiv = document.getElementById(elevator.id);  elevatorDiv.style.top = `${floor * 50}px`;
-  elevator.currentFloor = floor;}
-function findClosestElevator(floor) {
-  let closestElevator = null;  let closestDistance = Infinity;
-    elevators.forEach(elevator => {
-    const distance = Math.abs(elevator.currentFloor - floor);    if (distance < closestDistance) {
-      closestDistance = distance;      closestElevator = elevator;
-    }  });
-    return closestElevator;
+
+function moveToFloor(elevator, floor) {
+  const elevatorDiv = document.getElementById(elevator.id);
+  elevatorDiv.style.top = `${floor * 50}px`;
+  elevator.currentFloor = floor;
 }
-document.querySelectorAll('.floor').forEach(floorDiv => {  floorDiv.addEventListener('click', function() {
-    const floor = parseInt(this.dataset.floor);    const closestElevator = findClosestElevator(floor);
-    moveToFloor(closestElevator, floor);  });
+
+function findClosestElevator(floor) {
+  return elevators.reduce((closestElevator, elevator) => {
+    const distance = Math.abs(elevator.currentFloor - floor);
+    const closestDistance = Math.abs(closestElevator.currentFloor - floor);
+
+    return distance < closestDistance ? elevator : closestElevator;
+  }, elevators[0]);
+}
+
+document.querySelectorAll('.floor').forEach(floorDiv => {
+  floorDiv.addEventListener('click', function () {
+    const floor = parseInt(this.dataset.floor);
+    const closestElevator = findClosestElevator(floor);
+    moveToFloor(closestElevator, floor);
+  });
 });
